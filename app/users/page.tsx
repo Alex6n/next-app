396da14beb3d4a75aead123e8e6c5052c179aa45
Alex6n <hheikal.work@gmail.com>
2023-11-18 16:13:19 +0200

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,20 +7,13 @@ interface User {
 
 const UsersPage = async () => {
   
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-
-  // this is only in fetch function.. 
-  // to disable caching, this is good for data the changes frequently, to show fresh data always
-  // const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
-
-  // to keep data fresh for certain period of time, getting fresh data every 10 seconds
-  // const res = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } });
-  
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' });  
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{ new Date().toLocaleTimeString() }</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
